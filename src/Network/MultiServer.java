ae4f9e07a67e616529df3aa42eb7438c59e64ab0
TheKindlerofWildfires <mhock99@gmail.com>
2019-02-22 21:21:34 -0600

    Added Java's server shit

---
+++ src/Network/MultiServer.java
@@ -0,0 +1,26 @@
+package Network;
+import java.net.*;
+import java.io.*;
+
+public class MultiServer {
+    public static void main(String[] args) throws IOException {
+
+        /*if (args.length != 1) {
+            System.err.println("Usage: java KKMultiServer <port number>");
+            System.exit(1);
+        }*/
+
+        //int portNumber = Integer.parseInt(args[0]);
+        int portNumber = 1337;
+        boolean listening = true;
+
+        try (ServerSocket serverSocket = new ServerSocket(portNumber)) {
+            while (listening) {
+                new MultiServerThread(serverSocket.accept()).start();
+            }
+        } catch (IOException e) {
+            System.err.println("Could not listen on port " + portNumber);
+            System.exit(-1);
+        }
+    }
+}
