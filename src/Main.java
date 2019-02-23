27e67c03f485fda658558aa2f03bbae7a9a4eaa2
TheKindlerofWildfires <mhock99@gmail.com>
2019-02-22 19:28:47 -0600

    Camera

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import org.joml.Matrix4f;
+import org.joml.Vector3f;
 import org.lwjgl.glfw.GLFWVidMode;
 
 import static org.lwjgl.glfw.GLFW.*;
@@ -27,6 +28,8 @@ public class Main {
         glfwShowWindow(window);
         glfwMakeContextCurrent(window);
         GL.createCapabilities();
+
+        Camera camera = new Camera(640, 480);
         glEnable(GL_TEXTURE_2D);
 
         float[] vertices = new float[]{
@@ -51,10 +54,10 @@ public class Main {
         Shader shader = new Shader("shader");
         Texture tex = new Texture("./resources/TheVoid.png");
 
-        Matrix4f projection = new Matrix4f().ortho2D(-640/2, 640/2, -480/2, 480/2);
         Matrix4f scale = new Matrix4f().scale(64);
-        Matrix4f target =  new Matrix4f();
-        projection.mul(scale, target);
+        Matrix4f target = new Matrix4f();
+
+        camera.setPosition(new Vector3f(-100,0,0));
 
         while(!glfwWindowShouldClose(window)){
 
@@ -62,11 +65,12 @@ public class Main {
                 glfwSetWindowShouldClose(window, true);
             }
             glfwPollEvents();
+            target = scale;
 
             glClear(GL_COLOR_BUFFER_BIT);
             shader.bind();
             shader.setUniform("sampler",0);
-            shader.setUniform("projection",target);
+            shader.setUniform("projection",camera.getProjection().mul(target));
             tex.bind(0);
             model.render();
             glfwSwapBuffers(window);
